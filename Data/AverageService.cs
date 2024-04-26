b1a3497ab94b66aad895cb47951211f9789cdb84
Matthew Mc Daid <L00160463@atu.ie>
2024-04-26 17:29:28 +0100

    Final TouchUps before submissions, including adding historical Data for 3 and 4.
    Project Complete :)

--- Data/AverageService.cs
+++ Data/AverageService.cs
@@ -12,7 +12,7 @@ namespace WattWatcher.Data
         private readonly HttpClient _httpClient = new HttpClient();
         private readonly string _baseUri = "https://wattwatcher-pro-default-rtdb.firebaseio.com/";
 
-        public async Task<Dictionary<DateTime, (double AvgWattsCircuit1, double AvgWattsCircuit2, double AvgAmpsCircuit1, double AvgAmpsCircuit2, double AvgKwhCircuit1, double AvgKwhCircuit2)>> GetDailyAveragesAsync()
+        public async Task<Dictionary<DateTime, (double AvgWattsCircuit1, double AvgWattsCircuit2, double AvgWattsCircuit3, double AvgWattsCircuit4, double AvgAmpsCircuit1, double AvgAmpsCircuit2, double AvgAmpsCircuit3, double AvgAmpsCircuit4, double AvgKwhCircuit1, double AvgKwhCircuit2, double AvgKwhCircuit3, double AvgKwhCircuit4)>> GetDailyAveragesAsync()
         {
             string apiUrl = $"{_baseUri}averages.json";
             HttpResponseMessage response = await _httpClient.GetAsync(apiUrl);
@@ -30,14 +30,23 @@ namespace WattWatcher.Data
                         Date = key,
                         AverageCircuit1 = group.Average(x => x.Circuit1.AvgWatts),
                         AverageCircuit2 = group.Average(x => x.Circuit2.AvgWatts),
+                        AverageCircuit3 = group.Average(x => x.Circuit3.AvgWatts),
+                        AverageCircuit4 = group.Average(x => x.Circuit4.AvgWatts),
+
                         AvgAmpsCircuit1 = group.Average(x => x.Circuit1.AvgAmps),
                         AvgAmpsCircuit2 = group.Average(x => x.Circuit2.AvgAmps),
+                        AvgAmpsCircuit3 = group.Average(x => x.Circuit3.AvgAmps),
+                        AvgAmpsCircuit4 = group.Average(x => x.Circuit4.AvgAmps),
+
+
                         AvgKwhCircuit1 = group.Average(x => x.Circuit1.AvgKwh),
-                        AvgKwhCircuit2 = group.Average(x => x.Circuit2.AvgKwh)
+                        AvgKwhCircuit2 = group.Average(x => x.Circuit2.AvgKwh),
+                        AvgKwhCircuit3 = group.Average(x => x.Circuit3.AvgKwh),
+                        AvgKwhCircuit4 = group.Average(x => x.Circuit4.AvgKwh),
                     })
                 .ToDictionary(
                     x => x.Date,
-                    x => (x.AverageCircuit1, x.AverageCircuit2, x.AvgAmpsCircuit1, x.AvgAmpsCircuit2, x.AvgKwhCircuit1, x.AvgKwhCircuit2)
+                    x => (x.AverageCircuit1, x.AverageCircuit2, x.AverageCircuit3, x.AverageCircuit4, x.AvgAmpsCircuit1, x.AvgAmpsCircuit2, x.AvgAmpsCircuit3, x.AvgAmpsCircuit4, x.AvgKwhCircuit1, x.AvgKwhCircuit2, x.AvgKwhCircuit3, x.AvgKwhCircuit4)
                 );
 
             return dailyAverages;
@@ -47,6 +56,10 @@ namespace WattWatcher.Data
         {
             public CircuitData Circuit1 { get; set; }
             public CircuitData Circuit2 { get; set; }
+
+            public CircuitData Circuit3 { get; set; }
+
+            public CircuitData Circuit4 { get; set; }
         }
 
         private class CircuitData
@@ -57,4 +70,3 @@ namespace WattWatcher.Data
         }
     }
 }
-
